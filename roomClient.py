b948ca083ce294081b3f1430520f3ec2bfd149b7
Chris-Ch-R <chayangkun.r@ku.th>
2022-01-14 20:34:07 +0700

    change port

--- roomClient.py
+++ roomClient.py
@@ -9,7 +9,7 @@ import sys
 HEADER_LENGTH = 10
 
 IP = "127.0.0.1"
-PORT = 1234
+PORT = 12345
 my_username = input("Username: ")
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import select
 
 HEADER_LENGTH = 10
 IP = "127.0.0.1"
-PORT = 1234
+PORT = 12345
 
 # init server
 serverSocket = socket(AF_INET , SOCK_STREAM)
